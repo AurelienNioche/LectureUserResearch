208345c3c8b48c728862f5f5d4794548f2850f6a
Aurélien Nioche <nioche.aurelien@gmail.com>
2020-09-09 17:00:41 +0300

    update

--- main.py
+++ main.py
@@ -1,53 +1,41 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy.stats as stats
+
 import seaborn as sns
 
-np.random.seed(4)
+# import statsmodels.formula.api as smf
+#
+df = pd.read_excel("data/RR.xlsx",
+                   usecols=[0, 1, 2, 3],
+                   names=['Country', 'Year', 'Debt', 'Growth'])
 
-mean_1 = 150.0
-mean_2 = 200.0
-
-small_std = 10.0
-large_std = 50.0
-
-n = 100
-
-val1_small_std = np.random.normal(mean_1, scale=large_std, size=n)
-val2_small_std = np.random.normal(mean_2, scale=large_std, size=n)
-
-val1_large_std = np.random.normal(mean_1, scale=small_std, size=n)
-val2_large_std = np.random.normal(mean_2, scale=small_std, size=n)
-
-fig, axes = plt.subplots(ncols=2, nrows=2)
-
-i = 0
-for val1, val2 in zip((val1_small_std, val1_large_std), (val2_small_std, val2_large_std)):
-    print("i", i)
-
-    ax = axes[i, 0]
-    sns.distplot(val1, ax=ax, color="C0")
-    sns.distplot(val2, ax=ax, color="C1")
-
-    ax.axvline(np.mean(val1), label="mean", color='C0', lw=2, ls='--')
-    ax.axvline(np.mean(val2), label="mean", color='C1', lw=2, ls='--')
-
-    ax.set_ylabel("Proportion")
-    ax.set_xlabel("value")
-
-    ax = axes[i, 1]
-
-    df = pd.DataFrame({
-        "x": val1,
-        "y": val2
-    }).melt()
-
-    sns.barplot(x="variable", y="value", ax=ax, data=df, ci="sd")
-
-    ax.set_ylim(0, max(mean_1, mean_2) + large_std * 1.25)
-    ax.axhline(mean_1, ls='--', color='black', alpha=0.1, lw=2)
-    ax.axhline(mean_2, ls='--', color='black', alpha=0.1, lw=2)
-
-    i += 1
-
+df.to_csv("data/rr.csv", index=False)
+# df.head()
+#
+# df.plot.scatter(x="Debt", y="Growth", figsize=(16, 9))
+# plt.show()
+df = pd.read_csv(os.path.join("data", "rr.csv"))
+#
+df['DebtBin'] = pd.cut(df.Debt, bins=range(0, 250, 40), include_lowest=False)
+y = df.groupby('DebtBin').Growth.mean()
+print(type(list(y.index.values)[0]))
+print()
+print(list(y.values))
+print(type(y))
+x = [i.left + (i.right - i.left)/2 for i in y.index.values]
+print(x)
+ax = sns.barplot(x=x, y=y.values, palette="Blues_d")
+ax.set_xlabel("Debt")
+ax.set_ylabel("Growth")
+plt.tight_layout()
 plt.show()
+
+
+df.plot.scatter(x="Debt", y="Growth", figsize=(16, 9))
+# sns.displot(x="Debt", y="Growth", data=df, binwidth=3,)
+# plt.show()
+
+
