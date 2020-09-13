11be3d66ded9a32667fc79f17b7a3dcc23cffcc4
AurelienNioche <nioche.aurelien@gmail.com>
2020-09-13 18:39:00 +0300

    update

--- draft.py
+++ draft.py
@@ -4,3 +4,29 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import numpy as np
+
+
+
+m1, m2 = 100, 100
+sd1, sd2 = 10, 10
+n = 20
+
+seed = 0
+
+while True:
+    np.random.seed(seed)
+    x1 = np.random.normal(m1, scale=sd1, size=n)
+    x2 = np.random.normal(m2, scale=sd2, size=n)
+    print("x1", x1)
+    print("x2", x2)
+
+    thr = 0.05
+    t, p = stats.ttest_ind(x1, x2, equal_var=True)
+    can_reject = p < thr
+    print(f"t={t}, p={p}, can reject={can_reject}")
+
+    if can_reject:
+        print(seed)
+        break
+    else:
+        seed += 1
