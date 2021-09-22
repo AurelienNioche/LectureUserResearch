1bf3f15c1f52f3cc650f0b80ed7554493d9df098
AurelienNioche <nioche.aurelien@gmail.com>
2021-09-22 12:21:00 +0300

    update

--- convert_to_html.zsh
+++ convert_to_html.zsh
@@ -5,5 +5,8 @@ jupyter nbconvert --to exporter.exporter.CH_TOC lecture02-user-study-design.ipyn
 jupyter nbconvert --to exporter.exporter.CH_TOC lecture03-inference.ipynb --output docs/lecture03.html
 jupyter nbconvert --to exporter.exporter.CH_TOC lecture04-data-visualization.ipynb --output docs/lecture04.html
 echo "copying images..."
+rm -r docs/img
+rm -r docs/img_lec4
 cp -r img docs
+cp -r img_lec4 docs
 echo "done!"
